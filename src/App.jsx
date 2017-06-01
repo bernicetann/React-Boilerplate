f8e928a63b627b064d8a64c2434a6d8ee834975e
Bernice Tan <bernice.tann@gmail.com>
2017-06-01 00:05:28 +0000

    Displayed incoming messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,7 @@ import ChatBar from './Chatbar.jsx';
 
 const data = {
   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
+  messages: []
 };
 
 class App extends Component {
@@ -27,26 +16,31 @@ class App extends Component {
 
   componentDidMount() {
   console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-    }, 3000);
-  }
+  //connect Websocket to localhost 3001
+  var webSocket = new WebSocket("ws://localhost:3001");
+  webSocket.onopen = function (event) {
+      console.log("Connected to server");
+  };
+  //Store webSocket in this.socket.
+  this.socket = webSocket;
 
-  onNewPost(content, username) {
+  //This will rerender components and the new message should show up
+  this.socket.onmessage = (event) => {
+    console.log(event);
     this.setState({
-      messages: this.state.messages.concat({ id: this.state.messages.length+1,
-                                             username: username,
-                                             content: content
-                                           })
-     });
+      messages: this.state.messages.concat(JSON.parse(event.data))
+    });
+  }
   }
 
+  //When you press enter, you send the object with username/content to the server
+  onNewPost(content, username) {
+    this.socket.send(JSON.stringify({ username: username || this.state.currentUser.name,
+                                      content: content
+                                    }));
+  }
+
+  //Render the DOM
   render() {
     return (
       <div>
